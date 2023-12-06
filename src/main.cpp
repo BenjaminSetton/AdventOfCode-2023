99eacb1622939fcca855d1c25d09f73a5ec53d84
Benjamin Setton <benjasetton@gmail.com>
2023-12-05 23:22:01 -0600

    Day 5 complete!

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,9 @@
 #include <iostream>
 #include <vector>
 
-#include "day4/day4.h"
+#include "day5/day5.h"
 
-static const std::string inputFilePath = "../../src/day4/input4_2.txt";
+static const std::string inputFilePath = "../../src/day5/input5_2.txt";
 
 int main()
 {
@@ -26,7 +26,7 @@ int main()
 		input.push_back(buffer);
 	}
 
-	Day4_2 challenge;
+	Day5_2 challenge;
 	std::cout << "Output: " << challenge.Run(input) << std::endl;
 
 	fileHandle.close();
